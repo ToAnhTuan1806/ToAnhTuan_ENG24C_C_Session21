9b5af0b01cea6746728ece35df7d0c01e1fb0741
ToAnhTuan1806 <toanhtuan.18dz@gmail.com>
2024-12-24 21:58:34 +0700

    Add files via upload

---
+++ BT4ss21.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include<string.h>
+
+int main() {
+    FILE *fptr;
+    char str[100];
+
+    fptr = fopen("bt01.txt", "r");
+    if (fptr==NULL) {
+        printf("Loi mo file!\n");
+        return 1;
+    }
+	fgets(str, sizeof(str), fptr);
+	printf("Dong dau tien doc tu file: %s\n", str);
+	
+    fclose(fptr);
+
+
+	
+    return 0;
+}
+
